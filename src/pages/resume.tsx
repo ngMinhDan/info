07c812ea3427e7c423ba19bdf15190855bbf6295
MinhDan <nguyenmd.works@gmail.com>
2023-09-18 20:52:51 +0700

    edit resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -2,7 +2,7 @@ import { withMainLayoutPage } from '@/components/layouts';
 import { FunctionComponent } from 'react';
 
 const ResumePage: FunctionComponent = () => (
-  <iframe src="/media/MinhDan.pdf" className="h-screen w-full" />
+  <iframe src="/media/resume.pdf" className="h-screen w-full" />
 );
 
 export default withMainLayoutPage(ResumePage, {
